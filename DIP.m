a069edcf6f35f4491960d5696fc1b8994699625b
FigoHu <okjjhfj111@126.com>
2015-09-28 18:57:16 +0800

    changed2!

--- DIP.m
+++ DIP.m
@@ -1,4 +1,6 @@
 RGB_Image = imread('Rights.bmp');
 GRY_Image = rgb2gray(RGB_Image);
-BW_Image  = im2bw(RGB_Image);
-imshow(GRY_Image);
+%BW_Image  = im2bw(RGB_Image);
+p=imhist(GRY_Image,256)/numel(f);
+bar(1:1:256,p(1:1:256));
+%imshow(GRY_Image);
